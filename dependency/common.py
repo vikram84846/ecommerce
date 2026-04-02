aa4d33bf84a5a50ed5489b67f6cb98529b55cee2
vikram <vikram@snakescript.com>
2026-04-02 11:30:04 +0530

    used best practices for dependecies

--- dependency/common.py
+++ dependency/common.py
@@ -1,7 +1,13 @@
+from typing import AsyncGenerator
 from core.database import AsyncSessionLocal
+from sqlalchemy.ext.asyncio import AsyncSession
 
-async def get_db():
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
-        yield session
-        await session.close()
-
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
